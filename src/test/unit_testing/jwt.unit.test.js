4478a2495b6d64da30d17a8f0f47180b64ac9920
mdelahoz <mdelahoz@alumnos.inf.utfsm.cl>
2023-08-10 10:44:15 -0400

    implementacion de patron aaa

--- src/test/unit_testing/jwt.unit.test.js
+++ src/test/unit_testing/jwt.unit.test.js
@@ -1,15 +1,23 @@
 import { createAccessToken } from "../../src/libs/jwt.js";
 
-describe ("JWT", () => {
-    describe("createAccessToken", () => {
-      test("debe retornar un token", async () => {
-        const token = await createAccessToken({ id: 1 });
-        expect(typeof token).toBe("string");
-      }
-      );
-      test("debe retornar un error si no se le pasa un payload", async () => {
-        await expect(createAccessToken()).rejects.toThrow();
-      }
-      );
+//patron AAA, Ajustar, Actuar, Afirmar
+
+describe("JWT", () => {
+  describe("createAccessToken", () => {
+    test("debe crear un token", async () => {
+      //Ajustar
+      const usuario = {
+        id: 3,
+        username: Juan,
+      };
+      //actuar
+      const token = await createAccessToken({ usuario_id });
+      //afirmar
+      expect(typeof token).toBe("string");
     });
+    
+    test("debe producirse un error si no se le pasa un payload", async () => {
+      await expect(createAccessToken()).rejects.toThrow();
+    });
+  });
 });
